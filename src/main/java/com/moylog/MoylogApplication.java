cb60065f0db6a85fb08aed5c32efb7d0c311a240
jihwankim128 <jihwan1535@nate.com>
2026-04-26 05:03:53 +0900

    chore: 프로젝트 초기 설정 추가

---
+++ src/main/java/com/moylog/MoylogApplication.java
@@ -0,0 +1,12 @@
+package com.moylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoylogApplication.class, args);
+    }
+}
---
+++ src/test/java/com/moylog/MoylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
